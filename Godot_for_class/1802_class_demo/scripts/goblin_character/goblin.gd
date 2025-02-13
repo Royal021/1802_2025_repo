eb5f2690b91e63511c28f0985eb7f601fb937b6c
Will Martin <williambc76@hotmail.com>
2025-02-12 19:48:54 -0500

    added goblin and a projectile
    Added:
    Test Scene
    Goblin
    Arrow
    Fixes:
    na
    notes:
    na

---
+++ Godot_for_class/1802_class_demo/scripts/goblin_character/goblin.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+var arrow_premade = load("res://scripts/goblin_character/arrow.tscn") 
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	#if Input.is_action_just_pressed("jump") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
+
+	if Input.is_action_just_pressed("attack"):
+		var new_arrow = arrow_premade.instantiate()
+		$arrow_container.add_child(new_arrow)
+		new_arrow.global_position = $arrow_container.global_position
+		new_arrow.linear_velocity = transform.basis.z * 10
+		
+		
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	move_and_slide()
